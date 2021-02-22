9c99dc53393f52293bfa1b4851f9feca6227de88
AlexGrin1 <a_grynevich@mail.ru>
2021-02-22 18:16:55 +0300

    refactor: change layout js and css

--- Dynamic Landing Page/main.js
+++ Dynamic Landing Page/main.js
@@ -1,1 +1,17 @@
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const focus = document.getElementById("focus");
 
+function showTime() {
+  let today = new Date();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  hour = hour % 12 || 12;
+
+  time.innerHTML = `${hour}`;
+}
